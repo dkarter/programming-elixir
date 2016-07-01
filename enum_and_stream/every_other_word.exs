8c72b1aee2c7351df47d6d4991dadec144dde393
Dorian Karter <dkarter@gmail.com>
2016-07-01 00:26:00 -0500

    Add comments to EveryOtherWord module

--- enum_and_stream/every_other_word.exs
+++ enum_and_stream/every_other_word.exs
@@ -1,12 +1,13 @@
 defmodule EveryOtherWord do
   def print_every_other_word(filename) do
     File.stream!(filename)
-    |> Stream.filter(&(String.match?(&1, ~r/\w+/)))
-    |> Stream.map(&String.strip/1)
-    |> Stream.take_every(2)
+    |> Stream.filter(&(String.match?(&1, ~r/\w+/))) # remove empty lines
+    |> Stream.map(&String.strip/1) # remove trailing new lines
+    |> Stream.take_every(2) # interesting experiment: what would happen if I move this up
     |> Enum.into([])
   end
 end
 
 EveryOtherWord.print_every_other_word("sample.txt")
 |> Enum.each(&IO.puts/1)
+
